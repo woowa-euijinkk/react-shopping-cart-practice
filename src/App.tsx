7aa54338e8f257398a08f9f4764afd87ce43cd97
woowa-euijinkk <euijinkk@woowahan.com>
2025-03-26 13:17:54 +0900

    feat: react-router 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import { RecoilRoot } from "recoil";
-import CartPage from "./cart/CartPage";
+import { default as MainPage } from "./cart/CartPage";
+import CartPage from "./page-state/CartPage";
+import { BrowserRouter, Route, Routes } from "react-router";
+import OrderCheckPage from "./page-state/OrderCheckPage";
 
 function App() {
   return (
     <RecoilRoot>
-      <CartPage />
+      <BrowserRouter>
+        <PageRouter />
+      </BrowserRouter>
     </RecoilRoot>
+  );
+}
+
+function PageRouter() {
+  return (
+    <Routes>
+      <Route index element={<MainPage />} />
+      <Route path={"/cart"} element={<CartPage />} />
+      <Route path={"/order-check"} element={<OrderCheckPage />} />
+    </Routes>
   );
 }
 
